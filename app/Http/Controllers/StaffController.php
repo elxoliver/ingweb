9a8f6a28b1512e7b9370f4509fd66c2d2bbe5ab4
Xavier Díaz <33092817+xubylele@users.noreply.github.com>
2020-01-19 14:57:53 -0300

    Token Auth successfull

---
+++ app/Http/Controllers/StaffController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Staff;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class StaffController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+        try{
+            if(! $token = JWTAuth::attempt($credentials))
+                return response()->json(['error' => 'Invalid Credentials'], 400);
+            
+        }catch(JWTException $e){
+            return response()->json(['error' => 'Could not create a token'], 500);
+        }
+        return response()->json(compact('token'), 200);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+            if(!$user=JWTAuth::parseToken()->authenticate())
+                return response()->json(['error' => 'User not found', 404]);
+
+        } catch (TokenExpiredException $e) {
+                return response()->json(['token_expired'], 419);
+        } catch (TokenInvalidException $e) {
+                return response()->json(['token_invalid'], 400);
+        } catch (JWTException $e) {
+                return response()->json(['token_absent'], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            JWTAuth::invalidate($request->token);
+            return response()->json(['message' => 'Logout successfully'], 200);
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'An error has ocurred'
+            ], 500);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name'=>'required|string|max:45',
+            'last_name'=>'required|string|max:45',
+            'picture'=>'string',
+            'email'=>'required|email|unique:staff',
+            'store_id'=>'required|integer',
+            'active'=>'required|boolean',
+            'username'=>'required|string|max:16|unique:staff',
+            'password'=>'required|string|min:8|max:16',
+            'password_confirmation'=>'required|same:password'
+        ]);
+
+        if($validator->fails())
+            return response()->json($validator->errors()->toJson(), 400);
+
+        $staff = Staff::create([
+            'first_name' => $request->get('first_name'),
+            'last_name' => $request->get('last_name'),
+            'picture' => $request->get('picture'),
+            'email' => $request->get('email'),
+            'store_id' => $request->get('store_id'),
+            'active' => $request->get('active'),
+            'username' => $request->get('username'),
+            'password' => $request->get('password'),
+            'address_id' => 1
+        ]);
+
+        $token = auth()->login($staff);
+
+        return response()->json(compact('staff', 'token'), 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StaffController;
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('register', 'StaffController@register');
+Route::post('login', 'StaffController@login');
+
+
+
+Route::group(['middleware' => 'jwt.verify'], function(){
+    Route::post('logout', 'StaffController@logout');
 });
